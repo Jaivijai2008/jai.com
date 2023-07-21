acacec73b2373fcbe2d80c19de2b599ce3168516
Jaivijai2008 <132146661+Jaivijai2008@users.noreply.github.com>
2023-07-21 09:36:43 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,14 @@
+# #ignore this line
+# print("hello world" , end=" ")
+# print("jai vijai")
+# var1=23
+# var2 = 25
+# print(var1+var2)
+# fast1="22"
+# fast2="44"
+# print(fast1+fast2)
+# print(type(var1))
 
-print("hello world")
 
 
 
